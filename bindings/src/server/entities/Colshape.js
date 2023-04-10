f68bcaac5999abbbe2c78b149f2890e13c42b760
Artem Dzhemesiuk <programming@zziger.me>
2023-04-10 18:28:53 +0200

    Added missing file

---
+++ bindings/src/server/entities/Colshape.js
@@ -0,0 +1,88 @@
+import mp from '../../shared/mp';
+import * as alt from 'alt-server';
+import {_WorldObject} from './WorldObject';
+import {Pool} from '../Pool';
+
+const colshapeTypes = {
+    0: 'sphere',
+    1: 'tube',
+    2: 'circle',
+    3: 'cuboid',
+    4: 'rectangle',
+    5: 'checkpoint',
+    6: 'polygon'
+}
+
+export class _Colshape extends _WorldObject {
+    alt;
+
+    /** @param {alt.Colshape} alt */
+    constructor(alt) {
+        super(alt);
+        this.alt = alt;
+    }
+
+    isPointWithin(pos) {
+        return this.alt.isPointIn(pos);
+    }
+
+    get shapeType() {
+        return colshapeTypes[this.alt.colshapeType];
+    }
+}
+
+Object.defineProperty(alt.Colshape.prototype, 'mp', {
+    get() {
+        return this._mp ??= new _Colshape(this);
+    }
+});
+
+mp.Colshape = _Colshape;
+
+mp.colshapes = new Pool(() => alt.Colshape.all, alt.Colshape.getByID, () => alt.Colshape.all.length);
+
+mp.colshapes.newCircle = function(x, y, radius, dimension = 0) {
+    const shape = new alt.ColshapeCircle(x, y, radius);
+    shape.dimension = dimension;
+    shape.playersOnly = true;
+    return shape.mp;
+}
+
+mp.colshapes.newSphere = function(x, y, z, range, dimension = 0) {
+    const shape = new alt.ColshapeSphere(x, y, z, range);
+    shape.dimension = dimension;
+    shape.playersOnly = true;
+    return shape.mp;
+}
+
+mp.colshapes.newCuboid = function(x, y, z, width, depth, height, dimension = 0) {
+    const shape = new alt.ColshapeCuboid(x - width / 2, y - depth / 2, z - height / 2, x + width / 2, y + depth / 2, z + height / 2);
+    shape.dimension = dimension;
+    shape.playersOnly = true;
+    return shape.mp;
+}
+
+mp.colshapes.newRectangle = function(x, y, width, height, dimension = 0) {
+    const shape = new alt.ColshapeRectangle(x - width / 2, y - height / 2, x + width / 2, y + height / 2);
+    shape.dimension = dimension;
+    shape.playersOnly = true;
+    return shape.mp;
+}
+
+// RAGE MP docs don't have dimension arg here wtf
+mp.colshapes.newTube = function(x, y, z, height, range, dimension = 0) {
+    const shape = new alt.ColshapeCylinder(x, y, z, range, height);
+    shape.dimension = dimension;
+    shape.playersOnly = true;
+    return shape.mp;
+}
+
+alt.on('entityEnterColshape', (shape, ent) => {
+    if (!(ent instanceof alt.Player)) return;
+    mp.events.dispatch('playerEnterColshape', ent.mp, shape.mp);
+});
+
+alt.on('entityLeaveColshape', (shape, ent) => {
+    if (!(ent instanceof alt.Player)) return;
+    mp.events.dispatch('playerExitColshape', ent.mp, shape.mp);
+});
