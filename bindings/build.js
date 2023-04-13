b4398e7a4e15ba297e29945c7cfdcc7fe0418133
Artem Dzhemesiuk <programming@zziger.me>
2023-04-13 13:57:54 +0200

    Changed client build

--- bindings/build.js
+++ bindings/build.js
@@ -1,2 +1,46 @@
-require('./build/client.js');
-require('./build/server.js');
+const esbuild = require('esbuild')
+const { globalExternals } = require('@fal-works/esbuild-plugin-global-externals');
+
+esbuild.build({
+    entryPoints: ['src/server/index.js'],
+    bundle: true,
+    outdir: 'dist',
+    entryNames: 'server',
+    format: 'cjs',
+    target: 'es2017',
+    sourcemap: 'inline',
+    plugins: [globalExternals({
+        'alt-server': {
+            varName: 'alt',
+            type: 'cjs'
+        },
+        'alt-shared': {
+            varName: 'alt',
+            type: 'cjs'
+        }
+    })]
+});
+
+esbuild.build({
+    entryPoints: ['src/client/index.js'],
+    bundle: true,
+    outdir: 'dist',
+    entryNames: 'client',
+    format: 'cjs',
+    target: 'es2017',
+    sourcemap: 'inline',
+    plugins: [globalExternals({
+        'alt-client': {
+            varName: 'alt',
+            type: 'cjs'
+        },
+        'alt-shared': {
+            varName: 'alt',
+            type: 'cjs'
+        },
+        'natives': {
+            varName: 'native',
+            type: 'cjs'
+        }
+    })]
+});
