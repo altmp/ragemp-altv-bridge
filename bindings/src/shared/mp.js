bd53a68c41cf049294651546d2c72238346f4177
Vektor <whiteman.chr@hotmail.com>
2023-04-13 14:41:34 +0200

    add mp.joaat

--- bindings/src/shared/mp.js
+++ bindings/src/shared/mp.js
@@ -1,4 +1,7 @@
+import * as alt from 'alt-shared';
+
 const mp = {
-    prefix: globalThis.overrideBridgePrefix ?? '$bridge$' // prefix for bridge events and metas
+    prefix: globalThis.overrideBridgePrefix ?? '$bridge$', // prefix for bridge events and metas
+    joaat: (model) => alt.hash(model)
 };
 export default mp;
