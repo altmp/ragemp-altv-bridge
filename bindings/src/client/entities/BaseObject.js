7a30c3a879bea95b4f848f92a1f0ba1804f41757
Artem Dzhemesiuk <programming@zziger.me>
2023-05-03 01:15:21 +0200

    Fixed client baseobject JSON serialization

--- bindings/src/client/entities/BaseObject.js
+++ bindings/src/client/entities/BaseObject.js
@@ -2,4 +2,8 @@ export class _BaseObject {
     get isMpWrapper() {
         return true;
     }
+
+    toJSON() {
+        return JSON.stringify(Object.fromEntries(Object.entries(this).filter(e => e[0] !== 'alt')))
+    }
 }
