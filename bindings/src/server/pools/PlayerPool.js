38be7f965160042397f85d714625d006693e4276
Artem Dzhemesiuk <programming@zziger.me>
2023-06-01 14:19:32 +0200

    Events debug 3

--- bindings/src/server/pools/PlayerPool.js
+++ bindings/src/server/pools/PlayerPool.js
@@ -13,18 +13,22 @@ export class PlayerPool extends ServerPool {
     //mp.players.call(Array players, String eventName[, Array Arguments]);
     call(arg1, args1 = [], args2 = []) {
         if(typeof arg1 === 'string') {
+            console.log('EMIT CALL ALL CLIENTS', this.alt.id, arg1, args1);
             alt.emitAllClients(arg1, ...argsToAlt(args1));
         } else if(typeof arg1 === 'object' && Array.isArray(arg1)) {
             const players = arg1.map(p => p.alt).filter(Boolean);
+            console.log('EMIT CALL CLIENTS ARR', players.map(e => e.id), args1, args2);
             if (players.length) alt.emitClient(players, args1, ...argsToAlt(args2));
         }
     }
 
     callUnreliable(arg1, args1 = [], args2 = []) {
         if(typeof arg1 === 'string') {
+            console.log('EMIT CALL ALL CLIENTS UNRELIABLE', this.alt.id, arg1, args1);
             (mp._forceReliable ? alt.emitAllClients : alt.emitAllClientsUnreliable)(arg1, ...argsToAlt(args1));
         } else if(typeof arg1 === 'object' && Array.isArray(arg1)) {
             const players = arg1.map(p => p.alt).filter(Boolean);
+            console.log('EMIT CALL CLIENTS UNRELIABLE ARR', players.map(e => e.id), args1, args2);
             if (players.length) (mp._forceReliable ? alt.emitClient : alt.emitClientUnreliable)(players, args1, ...argsToAlt(args2));
         }
     }
@@ -33,12 +37,14 @@ export class PlayerPool extends ServerPool {
     callInRange(pos, range, event, args) {
         const rangeSquared = range ** 2;
         const players = alt.Player.all.filter(e => e.pos.distanceToSquared(pos) <= rangeSquared);
+        console.log('EMIT CALL CLIENTS IN RANGE ARR', players.map(e => e.id), range, event, args);
         if (players.length) alt.emitClient(players, event, ...argsToAlt(args));
     }
 
     callInDimension(dimension, event, args) {
         dimension = mpDimensionToAlt(dimension);
         const players = alt.Player.all.filter(e => e.dimension === dimension);
+        console.log('EMIT CALL CLIENTS IN DIMENSION ARR', players.map(e => e.id), dimension, event, args);
         if (players.length) alt.emitClient(players, event, ...argsToAlt(args));
     }
     /*
