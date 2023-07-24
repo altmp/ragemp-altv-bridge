17d3e5dcff2cc2b538fcad2b48aee0fb1caea2d7
Artem Dzhemesiuk <programming@zziger.me>
2023-07-24 14:05:21 +0200

    Fixed client checkpoints

--- bindings/src/client/entities/Checkpoint.js
+++ bindings/src/client/entities/Checkpoint.js
@@ -40,7 +40,8 @@ mp.Checkpoint = _Checkpoint;
 mp.checkpoints = new ClientPool(EntityGetterView.fromClass(alt.Checkpoint));
 
 mp.checkpoints.new = function (type, pos, radius, options = {}) {
-    const checkpoint = new alt.Checkpoint(type, pos, options.nextPos ?? new alt.Vector3(0, 0, 0), radius, 100, options.color ? new alt.RGBA(...options.color) : alt.RGBA.red, mp.streamingDistance);
+    const color = options.color ? new alt.RGBA(...options.color) : alt.RGBA.red;
+    const checkpoint = new alt.Checkpoint(type, pos, options.nextPos ?? new alt.Vector3(0, 0, 0), radius, 100, color, color, mp.streamingDistance);
     if ('dimension' in checkpoint) checkpoint.dimension = mpDimensionToAlt(options.dimension);
     return checkpoint.mp;
 };
