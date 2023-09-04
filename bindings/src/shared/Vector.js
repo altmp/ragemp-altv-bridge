7188347979f4b5972fe074eda8820a3332c28dab
Artem Dzhemesiuk <programming@zziger.me>
2023-09-04 12:34:01 +0200

    Fixed vector behavior on non-number values

--- bindings/src/shared/Vector.js
+++ bindings/src/shared/Vector.js
@@ -2,21 +2,29 @@ import mp from './mp.js';
 
 class _Vector3 {
     constructor(x, y, z) {
-        if (typeof x === 'number'){
-            this.x = x || 0.0;
-            this.y = y || 0.0;
-            this.z = z || 0.0;
-        } else if (typeof x === 'object') {
+        if (typeof x === 'object' && x != null) {
             if (Array.isArray(x)) {
-                this.x = x[0];
-                this.y = x[1];
-                this.z = x[2];
+                this.x = x[0] || 0;
+                this.y = x[1] || 0;
+                this.z = x[2] || 0;
             } else {
-                this.x = x.x;
-                this.y = x.y;
-                this.z = x.z;
+                this.x = x.x || 0;
+                this.y = x.y || 0;
+                this.z = x.z || 0;
             }
+        } else if (typeof x === 'number'){
+            this.x = x || 0;
+            this.y = y || 0;
+            this.z = z || 0;
+        } else {
+            this.x = 0;
+            this.y = 0;
+            this.z = 0;
         }
+
+        if (isNaN(this.x)) this.x = 0;
+        if (isNaN(this.y)) this.y = 0;
+        if (isNaN(this.z)) this.z = 0;
     }
 
     negative() {
