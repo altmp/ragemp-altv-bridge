458d2defe86239ae718498f57bbdca2dc30acb47
Artem Dzhemesiuk <programming@zziger.me>
2023-09-19 19:47:55 +0200

    Better entity logging

--- bindings/src/client/entities/BaseObject.js
+++ bindings/src/client/entities/BaseObject.js
@@ -1,13 +1,23 @@
+import alt from 'alt-client';
+
 export class _BaseObject {
     get isMpWrapper() {
         return true;
     }
 
     toJSON() {
-        return JSON.stringify(Object.fromEntries(Object.entries(this).filter(e => e[0] !== 'alt')))
+        return JSON.stringify(Object.fromEntries(Object.entries(this).filter(e => e[0] !== 'alt')));
     }
 
     forceStreamingUpdate() {
 
+    }
+
+    toString() {
+        return `${this.constructor.name}<${this.id ?? -1}, ${this.remoteID ?? -1}>`;
+    }
+
+    [alt.Utils.inspect.custom]() {
+        return this.toString();
     }
 }
--- bindings/src/server/entities/BaseObject.js
+++ bindings/src/server/entities/BaseObject.js
@@ -1,5 +1,15 @@
+import util from 'util';
+
 export class _BaseObject {
     get isMpWrapper() {
         return true;
+    }
+
+    toString() {
+        return `${this.constructor.name}<${this.id ?? -1}>`;
+    }
+
+    [util.inspect.custom]() {
+        return this.toString();
     }
 }
