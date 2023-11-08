c877e74d097a0cc0643cfc452442814d19447124
Artem Dzhemesiuk <programming@zziger.me>
2023-11-09 00:30:25 +0100

    Reverted spawn offset, fixed playAnimation

--- bindings/src/server/entities/Player.js
+++ bindings/src/server/entities/Player.js
@@ -340,7 +340,7 @@ export class _Player extends _Entity {
     // TODO: playScenario
 
     playAnimation(dict, name, speed, flag) {
-        this.alt.playAnimation(dict, name, speed, speed, -1, flag, 1, false, false, false);
+        this.alt.playAnimation(dict, name, speed, speed, -1, flag, 0, false, false, false);
     }
 
     outputChatBox(message) {
@@ -424,7 +424,7 @@ export class _Player extends _Entity {
 
     spawn(pos) {
         this.alt.removeAllWeapons();
-        this.alt.spawn({ x: pos.x, y: pos.y, z: pos.z - 0.8 });
+        this.alt.spawn({ x: pos.x, y: pos.y, z: pos.z - 1 });
         mp.events.dispatchLocal('playerSpawn', this);
     }
 
