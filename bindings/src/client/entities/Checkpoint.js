7049689cccffde80e5d167fd2fa71cf7992e6e8b
Artem Dzhemesiuk <programming@zziger.me>
2023-11-09 01:58:56 +0100

    Fixed client checkpoint direction

--- bindings/src/client/entities/Checkpoint.js
+++ bindings/src/client/entities/Checkpoint.js
@@ -41,7 +41,7 @@ mp.checkpoints = new ClientPool(EntityGetterView.fromClass(alt.Checkpoint));
 
 mp.checkpoints.new = function (type, pos, radius, options = {}) {
     const color = options.color ? new alt.RGBA(...options.color) : alt.RGBA.red;
-    const checkpoint = new alt.Checkpoint(type, pos, options.nextPos ?? new alt.Vector3(0, 0, 0), radius, 100, color, color, mp.streamingDistance);
+    const checkpoint = new alt.Checkpoint(type, pos, options.direction ?? new alt.Vector3(0, 0, 0), radius, 100, color, color, mp.streamingDistance);
     if ('dimension' in checkpoint) checkpoint.dimension = mpDimensionToAlt(options.dimension);
     return checkpoint.mp;
 };
