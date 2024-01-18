f9a7c8122c2225a08eb3485f4ef3a0526bf0ec54
Artem Dzhemesiuk <programming@zziger.me>
2024-01-18 16:42:42 +0100

    fix: mp.storage instance preservation

--- bindings/src/client/statics/Storage.js
+++ bindings/src/client/statics/Storage.js
@@ -4,39 +4,63 @@ import mp from '../../shared/mp.js';
 class _Storage {
     constructor() {
         alt.on('resourceStop', () => {
-            alt.LocalStorage.save();
+            this.flush();
         });
     }
 
     flush() {
+        for (const key in this.#dataCache)
+            alt.LocalStorage.set(key, this.#dataCache[key]);
+        for (const key in this.#sessionDataCache)
+            alt.setMeta(key, this.#sessionDataCache[key]);
         alt.LocalStorage.save();
     }
 
+    #sessionDataCache = new Map;
+    #sessionData;
     get sessionData() {
         const obj = {};
-        return this.#data ??= new Proxy({}, {
+        return this.#sessionData ??= new Proxy({}, {
             get(_, key) {
                 if (typeof key != 'string') return obj[key];
-                return alt.getMeta(key);
+
+                if (!this.#sessionDataCache.has(key)) {
+                    const instance = alt.getMeta(key);
+                    this.#sessionDataCache.set(key, instance);
+                }
+
+                return this.#sessionDataCache.get(key);
             },
             set(_, key, value) {
                 if (typeof key != 'string') return;
+
+                this.#sessionDataCache.set(key, value);
                 alt.setMeta(key, value);
                 return true;
             }
         });
     }
 
+
+    #dataCache = new Map;
     #data;
     get data() {
         const obj = {};
         return this.#data ??= new Proxy({}, {
-            get(_, key) {
+            get: (_, key) => {
                 if (typeof key != 'string') return obj[key];
-                return alt.LocalStorage.get(key);
+
+                if (!this.#dataCache.has(key)) {
+                    const instance = alt.LocalStorage.get(key);
+                    this.#dataCache.set(key, instance);
+                }
+
+                return this.#dataCache.get(key);
             },
-            set(_, key, value) {
+            set: (_, key, value) => {
                 if (typeof key != 'string') return;
+
+                this.#dataCache.set(key, value);
                 alt.LocalStorage.set(key, value);
                 return true;
             }
