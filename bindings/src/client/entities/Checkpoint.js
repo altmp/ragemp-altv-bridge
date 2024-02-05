2cf51abc1e6c696d087fb52a3460cda6d314efe4
Artem Dzhemesiuk <programming@zziger.me>
2024-02-05 23:53:08 +0100

    fix: checkpoint radius

--- bindings/src/client/entities/Checkpoint.js
+++ bindings/src/client/entities/Checkpoint.js
@@ -205,7 +205,7 @@ mp.checkpoints = new ClientPool(
 
 mp.checkpoints.new = function (type, pos, radius, options = {}) {
     const color = options.color ? new alt.RGBA(...options.color) : alt.RGBA.red;
-    const checkpoint = new alt.Checkpoint(type, pos, options.direction ?? new alt.Vector3(0, 0, 0), radius, 100, color, color, mp.streamingDistance);
+    const checkpoint = new alt.Checkpoint(type, pos, options.direction ?? new alt.Vector3(0, 0, 0), radius / 2, 100, color, color, mp.streamingDistance);
     if ('dimension' in checkpoint) checkpoint.dimension = mpDimensionToAlt(options.dimension);
     return checkpoint.mp;
 };
