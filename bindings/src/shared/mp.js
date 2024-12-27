8799ca56750899d7bc8f23b478932ad194b2a231
OlegiMiami <zlatalove96@gmail.com>
2024-12-27 18:17:18 +0300

    chore: change _disableRawEmits state

--- bindings/src/shared/mp.js
+++ bindings/src/shared/mp.js
@@ -17,7 +17,7 @@ const mp = {
     _measureTimers: false, // false or threshold in ms
     _measureEvents: false, // false or threshold in ms
     _shareVariablesBetweenResources: false,
-    _disableRawEmits: true,
+    _disableRawEmits: false,
     get _tickCount() {
         return tickCount;
     },
